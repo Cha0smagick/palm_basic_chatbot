16aa08e9b1b93bee02cb38b9fb304722e5938c8b
Cha0smagick <alekos20@gmail.com>
2023-12-06 14:38:19 -0500

    Update chatbot_esp_eng_streamlit.py

--- chatbot_esp_eng_streamlit.py
+++ chatbot_esp_eng_streamlit.py
@@ -3,7 +3,9 @@ from deep_translator import GoogleTranslator
 import google.generativeai as palm
 import re
 
-# L.A.U.R.A : Lingüista Artificial Unificada de Respuestas Automaticas
+# L.A.U.R.A: Lingüista Artificial Unificada de Respuestas Automaticas
+
+error_flag = False  # Global variable to track error display
 
 def clean_text(text):
     # Clean punctuation and special characters using regular expressions
@@ -16,6 +18,8 @@ def translate_text(text, target_language='en'):
     return translation
 
 def generate_response(cleaned_input, model):
+    global error_flag  # Use the global error_flag variable
+
     try:
         # Generate response using the model
         completion = palm.generate_text(
@@ -31,7 +35,19 @@ def generate_response(cleaned_input, model):
         return translated_output
 
     except Exception as e:
-        return f"Error: {e}\nDisculpa, soy una inteligencia artificial que aún se encuentra en desarrollo y está en fase alfa. En este momento no puedo responder a tu pregunta adecuadamente, pero en el futuro seré capaz de hacerlo."
+        error_message = str(e)
+        if "text must be a valid text with maximum 5000 character" in error_message and not error_flag:
+            error_response = ("La pregunta que está realizando puede que vaya en contra de las políticas de Google Bard e INIF. "
+                              "Por favor, reformule su pregunta sin temas no permitidos o pregunte algo diferente. "
+                              "Para más información consulte: https://policies.google.com/terms/generative-ai/use-policy "
+                              "o www.inif.com.co/laura-chatbot/use-policy")
+            st.error(error_response)
+            error_flag = True  # Set the error_flag to True after displaying the error message
+            return error_response
+        else:
+            error_response = f"Error: {error_message}\nDisculpa, soy una inteligencia artificial que aún se encuentra en desarrollo y está en fase alfa. En este momento no puedo responder a tu pregunta adecuadamente, pero en el futuro seré capaz de hacerlo."
+            st.error(error_response)
+            return error_response
 
 def main():
     st.title("Chatbot INIF - L.A.U.R.A.")
@@ -64,16 +80,19 @@ def main():
 
         # Add the command to act as an INIF informative chatbot
         cleaned_input = (
-            "Acts as an informative chatbot called L.A.U.R.A working for a fraud prevention company called the National Institute of Fraud Research and Prevention (INIF). www.inif.com.co "
-            "Your task is to answer the following question in the most accurate manner, act as a woman for more friendly manners. do not invent information, only give me information you know"
-            "The question you must respond to is as follows: " + cleaned_input
+            "Acts as an informative data analyst female chatbot called L.A.U.R.A working for a fraud prevention company called the National Institute of Fraud Research and Prevention (INIF). www.inif.com.co"
+            " Your task is to answer the following question. do not put or use fictional information, only give accurate and true information, as a scientist will do it"
+            " if you dont know something, just say it, dont invent nothing. The question you must respond to is as follows: " + cleaned_input
         )
 
         # Generate the response
         translated_output = generate_response(cleaned_input, model)
 
-        # Display the generated response
-        st.write("Respuesta:", translated_output)
+        # Display the generated response in green or red based on the content
+        if "La pregunta que está realizando puede que vaya en contra de las políticas de Google Bard e INIF." in translated_output:
+            st.error(translated_output)
+        else:
+            st.success(translated_output)
 
 if __name__ == "__main__":
     main()
